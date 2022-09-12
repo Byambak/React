907081193df8db19d9f495e97af7fa9870580d73
Reslyzz <doidoo0611@gmail.com>
2022-09-12 16:17:14 +0800

    asdiujhniuo
    sadsadasdasdas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const name =null;
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {2+2}!</h1>
+      {name ? (
+      <>
+      test
+      </>): (
+        <>
+        <h1>test</h1>
+        <h2>There is no no name</h2>
+        
+        </>
+      )}
+  
+    
     </div>
+    
   );
+  
 }
 
-export default App;
+
+export default App; 
